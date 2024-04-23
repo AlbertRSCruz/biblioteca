61c732b20e19a6a78ce7eca6ca627cc048fd7980
albertdevsoftware <albertrenanscruz@gmail.com>
2024-04-23 15:05:03 -0300

    Criada classes

---
+++ src/Endereco.java
@@ -0,0 +1,20 @@
+public class Endereco {
+    private String endereco;
+    private int cep;
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+}
---
+++ src/Locacao.java
@@ -0,0 +1,20 @@
+public class Locacao {
+    private int dataRetirada;
+    private int dataDevolucao;
+
+    public int getDataRetirada() {
+        return dataRetirada;
+    }
+
+    public void setDataRetirada(int dataRetirada) {
+        this.dataRetirada = dataRetirada;
+    }
+
+    public int getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(int dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+}
