305afe9987c1364ce964c2e36cfc316d8d6b10c6
albertdevsoftware <albertrenanscruz@gmail.com>
2024-05-13 22:02:24 -0300

    Override

--- src/Usuario.java
+++ src/Usuario.java
@@ -36,4 +36,15 @@ public class Usuario {
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", codigo=" + codigo +
+                ", cpf=" + cpf +
+                ", endereco=" + endereco +
+                '}';
+    }
 }
+
